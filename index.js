33587ad909b3c05b543086745bdc29bab5182813
Vlad Synkov <vlad@statsbot.co>
2018-06-25 14:11:14 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+import express from 'express'
+import path from 'path'
+import fs from 'fs'
+import m from 'moment'
+import { reverse, all, propEq, merge, addIndex, reduce, assoc, map } from 'ramda'
+import render from './render'
+import questions from './questions'
+const app = express()
+
+app.set('view engine', 'slm');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+app.use((err, request, response, next) => {
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+const displayPage = (res, result) => {
+  const data = JSON.parse(fs.readFileSync("data.json"))
+  res.render('index', merge(result || {}, {
+    data: reverse(data.map(render)),
+    questions
+  }))
+}
+
+app.get('/', (req, res) => displayPage(res))
+
+const valid = qs => qs && qs.length == 6 && all(propEq('length', 3), qs)
+
+app.post('/', (req, res) => {
+  console.debug(req.body)
+
+  let result = {}
+
+  if (!valid(req.body.questions)) {
+    displayPage(res, { error: "Invalid data" })
+    return
+  }
+
+  const parseAns = map(parseInt)
+
+  const newItem = addIndex(reduce)(
+    (acc, item, i) => assoc('l' + (i+1), parseAns(item), acc),
+    {},
+    req.body.questions
+  )
+  newItem.date = m().format('YYYY-MM-DD HH:mm')
+
+  console.debug("new item", newItem)
+
+  fs.readFile('./data.json', 'utf8', (err, json) => {
+    if (err){
+      console.log(err)
+      return
+    }
+
+    const data = JSON.parse(json) //now it an object
+    data.push(newItem) //add some data
+    console.debug(JSON.stringify(data, null, 2))
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', () => {})
+    console.log("written ok")
+    displayPage(res, {})
+  })
+})
+
+app.listen(3000)
+
---
+++ render.js
@@ -0,0 +1,64 @@
+import {map, pipe, filter, prop, addIndex, compose,
+  indexOf, sum, range} from 'ramda';
+import data from './data';
+
+const hexagramIndexes = [
+  [1, 43, 14, 34, 9, 5, 26, 11],
+  [10, 58, 38, 54, 61, 60, 41, 19],
+  [13, 49, 30, 55, 37, 63, 22, 36],
+  [25, 17, 21, 51, 42, 3, 27, 24],
+  [44, 28, 50, 32, 57, 48, 18, 46],
+  [6, 47, 64, 40, 59, 29, 4, 7],
+  [33, 31, 56, 62, 53, 39, 52, 15],
+  [12, 45, 35, 16, 20, 8, 23, 2]
+];
+const trigrams = [
+  [true, true, true],
+  [false, true, true],
+  [true, false, true],
+  [false, false, true],
+  [true, true, false],
+  [false, true, false],
+  [true, false, false],
+  [false, false, false],
+];
+const ansToLine = ans => ({
+    line: sum(ans) % 2 != 0,
+    strong: sum(ans) % 3 ==0
+});
+
+const hexNumber = data => {
+    const triTop = indexOf(
+          map(prop('line'), [
+                  ansToLine(data.l6),
+                  ansToLine(data.l5),
+                  ansToLine(data.l4)]),
+          trigrams
+        );
+    const triBottom = indexOf(
+          map(prop('line'), [
+                  ansToLine(data.l3),
+                  ansToLine(data.l2),
+                  ansToLine(data.l1)]),
+          trigrams
+        );
+    return hexagramIndexes[triBottom][triTop];
+};
+
+const indexMap = addIndex(map);
+const getStrong = data => pipe(
+  map(i => ansToLine(data['l' + i])),
+  indexMap((l, i) => ({val: l.strong, index: i+1})),
+  filter(prop('val')),
+  map(prop('index'))
+)(range(1, 7));
+
+const render = data => ({
+  date: data.date,
+  number: hexNumber(data),
+  symbol: String.fromCharCode(0x4DC0 + hexNumber(data) - 1),
+  strong: getStrong(data)
+});
+
+export default render;
+
