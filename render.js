dd5fc77921c45e5cb82e0dc3bb5c9630a65d2240
Vlad Synkov <himself@vladsynkov.ru>
2018-07-25 14:21:54 +0300

    add newNumber

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 import {map, pipe, filter, prop, addIndex, compose,
-  indexOf, sum, range} from 'ramda';
+  indexOf, sum, range} from 'ramda'
 
 const hexagramIndexes = [
   [1, 43, 14, 34, 9, 5, 26, 11],
@@ -10,7 +10,7 @@ const hexagramIndexes = [
   [6, 47, 64, 40, 59, 29, 4, 7],
   [33, 31, 56, 62, 53, 39, 52, 15],
   [12, 45, 35, 16, 20, 8, 23, 2]
-];
+]
 const trigrams = [
   [true, true, true],
   [false, true, true],
@@ -20,44 +20,65 @@ const trigrams = [
   [false, true, false],
   [true, false, false],
   [false, false, false],
-];
+]
 const ansToLine = ans => ({
-    line: sum(ans) % 2 != 0,
-    strong: sum(ans) % 3 ==0
-});
+  line: sum(ans) % 2 != 0,
+  strong: sum(ans) % 3 == 0,
+  newLine: !!(sum(ans) % 3 == 0 ^ sum(ans) % 2 != 0)
+})
 
-const hexNumber = data => {
-    const triTop = indexOf(
-          map(prop('line'), [
-                  ansToLine(data.l6),
-                  ansToLine(data.l5),
-                  ansToLine(data.l4)]),
-          trigrams
-        );
-    const triBottom = indexOf(
-          map(prop('line'), [
-                  ansToLine(data.l3),
-                  ansToLine(data.l2),
-                  ansToLine(data.l1)]),
-          trigrams
-        );
-    return hexagramIndexes[triBottom][triTop];
-};
+const getTrigrams = data => ({
+  top: indexOf(
+    map(prop('line'), [
+      ansToLine(data.l6),
+      ansToLine(data.l5),
+      ansToLine(data.l4)]),
+    trigrams
+  ),
+  bottom: indexOf(
+    map(prop('line'), [
+      ansToLine(data.l3),
+      ansToLine(data.l2),
+      ansToLine(data.l1)]),
+    trigrams
+  ),
+  newTop: indexOf(
+    map(prop('newLine'), [
+      ansToLine(data.l6),
+      ansToLine(data.l5),
+      ansToLine(data.l4)]),
+    trigrams
+  ),
+  newBottom: indexOf(
+    map(prop('newLine'), [
+      ansToLine(data.l3),
+      ansToLine(data.l2),
+      ansToLine(data.l1)]),
+    trigrams
+  ),
+})
 
-const indexMap = addIndex(map);
+const indexMap = addIndex(map)
+
 const getStrong = data => pipe(
   map(i => ansToLine(data['l' + i])),
   indexMap((l, i) => ({val: l.strong, index: i+1})),
   filter(prop('val')),
   map(prop('index'))
-)(range(1, 7));
+)(range(1, 7))
 
-const render = data => ({
-  date: data.date,
-  number: hexNumber(data),
-  symbol: String.fromCharCode(0x4DC0 + hexNumber(data) - 1),
-  strong: getStrong(data)
-});
+const render = data => {
+  const trigrams = getTrigrams(data)
+  const number = hexagramIndexes[trigrams.bottom][trigrams.top]
 
-export default render;
+  return {
+    date: data.date,
+    number,
+    newNumber: hexagramIndexes[trigrams.newBottom][trigrams.newTop],
+    symbol: String.fromCharCode(0x4DC0 + number - 1),
+    strong: getStrong(data)
+  }
+}
+
+export default render
 
